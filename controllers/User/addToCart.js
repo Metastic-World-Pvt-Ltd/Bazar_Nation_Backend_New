0e7ef90125d7c7e6ff05949c433d95bb47ad5eb1
anujsahumeta <anuj@metasticworld.com>
2023-12-28 11:05:30 +0530

    modified add to cart item API

--- controllers/User/addToCart.js
+++ controllers/User/addToCart.js
@@ -1,22 +1,51 @@
 const Cart = require('../../models/Cart');
+const Product = require('../../models/Product');
 const uuid = require('uuid');
 const errorMessages = require('../../response/errorMessages');
 
 module.exports.addToCart = async function(req , res){
-    const {userId , productId , qty} =  req.body;
+try {
+    const {userId , productId , qty , bill} =  req.body;
 
     if(!userId || !productId){
         return res.status(400).json(errorMessages.ALL_FIELDS_REQUIRED);
     }
-    
+    const productData =  await Product.findOne({productId});
+    //console.log(productData);
     const itmeId = Date.now();
+    const items = {
+        itemId: {
+            productId:productData.productId,
+            name: productData.name,
+            productImage: productData.productImage,
+            description: productData.description,
+            brandName: productData.brandName,
+            price: productData.price,
+            mrp: productData.mrp,
+            subCategory: productData.subCategory,
+            category: productData.category,
 
-    const data = await Cart.create({
-        itmeId , userId , productId , qty
-    })
+        },
+        qty: qty,
+    
+      }
+    const isExist = await Cart.findOne({userId});
+    if(isExist){
+       const data = await Cart.findOneAndUpdate({userId},{
+        $push: { items: items } , bill:bill
+    },{new:true})
+    res.status(200).json(data); 
+    }else{
+        const data = await Cart.create({
+            userId , bill , items
+       })
+       res.status(200).json(data);
+    }
+} catch (error) {
+    return res.status(500).json(errorMessages.INTERNAL_ERROR)
+}
 
-    console.log(data);
 
-    res.status(200).json(data);
+    
 
 }
--- models/Cart.js
+++ models/Cart.js
@@ -1,25 +1,85 @@
 const mongoose =  require('mongoose');
 
-const cartData =  new mongoose.Schema({
-    itmeId:{
-        type:String,
-        required:true,
-    },
-    userId:{
-        type:String,
-    },
-    productId:{
-        type:String,
-        required:true,
-    },
-    qty:{
-        type:String,
-        required:true,
-    },
+// const cartData =  new mongoose.Schema({
+//     itmeId:{
+//         type:String,
+//         required:true,
+//     },
+//     userId:{
+//         type:String,
+//     },
+//     productId:{
+//         type:String,
+//         required:true,
+//     },
+//     qty:{
+//         type:String,
+//         required:true,
+//     },
 
 
-},{
+// },{
+//     timestamps:true
+// })
+
+// Define the item schema
+const itemSchema = new mongoose.Schema({
+    itemId: {
+      type: {
+        name: String,
+        productImage: String,
+        description: String,
+        brandName: String,
+        price: Number,
+        mrp: Number,
+        subCategory: String,
+        category: String,
+      },
+      required: true,
+    },
+    qty: {
+      type: Number,
+      required: true,
+    },
+
+  });
+  
+  // Define the cart schema
+  const cartSchema = new mongoose.Schema({
+    userId: {
+      type: String,
+      required: true,
+    },
+    items: [
+        {
+            itemId: {
+              type: {
+                productId:String,
+                name: String,
+                productImage: String,
+                description: String,
+                brandName: String,
+                price: Number,
+                mrp: Number,
+                subCategory: String,
+                category: String,
+              },
+              required: true,
+            },
+            qty: {
+              type: Number,
+              required: true,
+            },
+        
+          }
+    ],
+    bill: {
+      type: Number,
+      required: true,
+    },
+
+  },{
     timestamps:true
-})
+  });
 
-module.exports = mongoose.model('Cart', cartData);
+module.exports = mongoose.model('Cart', cartSchema);
